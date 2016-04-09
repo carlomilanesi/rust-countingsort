77df78f57abcde54431d6312bc22e2fe667bc881
cmilanesi <carlo.milanesi@libero.it>
2016-04-09 12:39:56 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct S {
     d: f64,
 }
 
-fn create_struct_vect(inp: &[i32]) -> Vec<S> {
+fn create_struct_vec(inp: &[i32]) -> Vec<S> {
     let mut out = Vec::new();
     for i in inp {
         out.push(S {s: String::from(""), d: *i as f64});
@@ -22,7 +22,7 @@ fn main() {
     counting_sort(&mut v1, 200, |x| (x + 100) as usize);
     assert_eq!([-34, -1, -1, -1, -1, 0, 0, 5, 5, 6, 8, 13, 40, 55, 59, 98], v1);
 
-    let mut v1s = create_struct_vect(&v1);
+    let mut v1s = create_struct_vec(&v1);
     counting_sort(&mut v1s, 200, |s| (s.d + 100.0) as usize);
     for i in 1..v1s.len() {
         assert!(v1s[i - 1].d <= v1s[i].d)
@@ -33,12 +33,8 @@ fn main() {
     for item in &mut v2 {
         *item = rand::thread_rng().gen_range(-100, 100);
     }
-    for _ in 0..4_000 {
-        let mut v3 = vec![0; v2.len()];
-        for i in 0..v2.len() {
-            v3[i] = v2[i];
-        }
-        //v3 = v2.clone();
+    for _ in 0..2_000_000 {
+        let mut v3 = v2.clone();
         counting_sort(&mut v3, 200, |x| (x + 100) as usize);
 
         //v3.sort();
